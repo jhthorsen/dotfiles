ec98f935d83c52fe62f31331bbdba67e3348cffb
jhthorsen <jhthorsen@1f5e4627-c88f-4690-b96a-af9c8ecf0c1f>
2008-01-06 12:43:09 +0000

    details, details...
    git-svn-id: file:///home/subversion/snippets@19 1f5e4627-c88f-4690-b96a-af9c8ecf0c1f

--- media/m4a2mp3.sh
+++ media/m4a2mp3.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-MPLAYER=mplayer
-LAME=lame
+#===========
+# m4a2mp3.sh
+#===========
 
-### m4a to mp3
-for m4a in *.m4a
-do
+MPLAYER="mplayer";
+LAME="lame;
 
-    mp3=`echo "$m4a" | sed -e 's/m4a$/mp3/'`
-    wav="$m4a.wav"
+
+### convert
+for m4a in *.m4a; do
+
+    mp3=`echo "$m4a" | sed -e 's/m4a$/mp3/'`;
+    wav="$m4a.wav";
 
     if [ ! -e "$mp3" ]; then
-        [ -e "$wav" ] && rm "$wav"
-	mkfifo "$wav" || exit 255
+        [ -e "$wav" ] && rm "$wav";
+        mkfifo "$wav" || exit 255;
         mplayer "$m4a" -ao pcm:file="$wav" &>/dev/null &
-        lame "$wav" "$mp3"
-        [ -e "$wav" ] && rm "$wav"
+        lame "$wav" "$mp3";
+        [ -e "$wav" ] && rm "$wav";
     fi
 done
 
