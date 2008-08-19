d7a1c2f96527797816e5f284cf771854695ca241
jhthorsen <jhthorsen@1f5e4627-c88f-4690-b96a-af9c8ecf0c1f>
2008-08-19 06:41:28 +0000

    Rewrote most of meta2makefile.pl. Added POD
    git-svn-id: file:///home/subversion/snippets@26 1f5e4627-c88f-4690-b96a-af9c8ecf0c1f

--- bin/meta2makefile.pl
+++ bin/meta2makefile.pl
@@ -4,42 +4,82 @@ use strict;
 use warnings;
 use YAML;
 
-my $meta = load_yaml() or die "No config";
+our $VERSION = 0.1;
+my $META;
 
-open(my $mkfile, ">", "Makefile.PL") or die $!;
-print $mkfile "$_;\n" for(
-    qq(use inc::Module::Install),
-    qq(name '$meta->{name}'),
-    qq(abstract '$meta->{abstract}'),
-    qq(author '$meta->{author}'),
-    qq(all_from '$meta->{version_from}'),
-    requires(),
-    recommends(),
-    test_requires(),
-    qq(WriteAll),
-);
-close $mkfile;
+chdir $ARGV[0] if($ARGV[0]);
+load_yaml();
+write_makefile();
+exit 0;
 
-sub recommends {
+=head1 NAME
+
+meta2makefile.pl - a META.yml to Makefile.PL generator
+
+=head1 VERSION
+
+0.1
+
+=head1 DESCRIPTION
+
+Change directory to the first argument, or looks for META.yml in the current
+directory.
+Takes the information from META.yml and creates Makefile.PL.
+Will overwrite existing Makefile.PL without asking!
+
+=head1 USAGE
+
+ $ meta2makefile.pl /path/to/my/module/
+
+=head1 FUNCTIONS
+
+=head2 write_makefile()
+
+Takes information from <$META> and writes Makefile.PL to the current
+directory.
+
+=cut
+
+sub write_makefile {
+    my $get = sub {
+        my($make_sub, $yaml_key) = @_;
+        return unless(ref $META->{$yaml_key} eq 'HASH');
+        return map {
+                   qq($make_sub '$_' => $META->{$yaml_key}{$_})
+               } keys %{ $META->{$yaml_key} };
+    };
+
+    open(my $mkfile, ">", "Makefile.PL")
+        or die "Could not write Makefile.PL: $!";
+
+    for(map { "$_;\n" }
+        qq(use inc::Module::Install),
+        qq(name '$META->{name}'),
+        qq(abstract '$META->{abstract}'),
+        qq(author '$META->{author}'),
+        qq(all_from '$META->{version_from}'),
+        $get->(requires => 'requires'),
+        $get->(test_requires => 'build_requires'),
+        qq(WriteAll),
+    ) {
+        print $mkfile $_;
+        print;
+    }
+
+    close $mkfile;
 }
 
-sub requires {
-    map {
-        qq(requires '$_' => $meta->{requires}{$_})
-    } keys %{ $meta->{requires} };
-}
+=head2 load_yml()
 
-sub test_requires {
-    map {
-        qq(test_requires '$_' => $meta->{build_requires}{$_})
-    } keys %{ $meta->{build_requires} };
-}
+Loads META.yml from the current directory and stores the datastructure in
+C<$META>.
+
+=cut
 
 sub load_yaml {
-	my $file = "META.yml";
 	my $yaml = q();
 
-    open(my $fh, "<", $file) or die "Could not read YAML: $!";
+    open(my $fh, "<", "META.yml") or die "Could not read META.yml: $!";
 
     LINE:
     while(my $line = readline $fh) {
@@ -49,7 +89,6 @@ sub load_yaml {
         $yaml .= $line;
     }
 
-    close $fh;
-    return Load($yaml);
+    $META = Load($yaml) or die "No meta-data!";
 }
 
