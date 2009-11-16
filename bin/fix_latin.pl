044d5c73190e94b3dd91c4673abc27839c0c50e1
Jan Henning Thorsen <jhthorsen@cpan.org>
2009-11-16 20:00:57 +0100

    add pod to fix_latin.pl

--- bin/fix_latin.pl
+++ bin/fix_latin.pl
@@ -1,41 +1,43 @@
 #!/usr/bin/perl
 
-for(@ARGV) {
-    my $old = $_;
-
-    s/\xE6/æ/g;
-    s/\xF8/ø/g;
-    s/\xE5/å/g;
-    s/\xC6/Æ/g;
-    s/\xD8/Ø/g;
-    s/\xC5/Å/g;
-
-    link $old, $_;
-    unlink $old if($old ne $_);
-}
-
-exit;
-__END__
-
 =head1 NAME
 
 fix_latin.pl - A perl script that renames latin files to utf8
 
 =head1 USAGE
 
- $ fix_latin.pl *
+ $ fix_latin.pl <file> [file2] ...;
 
 =head1 DESCRIPTION
 
-Renames æøå from latin encoding to utf8
+Rename files with latin "æøå" to utf8 "æøå".
 
-=head1 AUTHOR
+=cut
 
-Jan Henning Thorsen
+for(@ARGV) {
+    my $old = $_;
+    my $new = $_;
+
+    $new = s/\xE6/æ/g;
+    $new = s/\xF8/ø/g;
+    $new = s/\xE5/å/g;
+    $new = s/\xC6/Æ/g;
+    $new = s/\xD8/Ø/g;
+    $new = s/\xC5/Å/g;
+
+    if($old ne $new) {
+        link $old, $new;
+        unlink $old;
+    }
+}
 
 =head1 LICENSE
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - jhthorsen -at- cpan.org
 
 =cut
