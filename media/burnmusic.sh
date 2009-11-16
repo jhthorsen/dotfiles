9e65606e0b2a625bc9a857b13bb138ecbb6bf78a
Jan Henning Thorsen <jhthorsen@cpan.org>
2009-11-16 20:32:54 +0100

    cleaned up code

--- media/burnmusic.sh
+++ media/burnmusic.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-
-### nothing to burn
 if [ $# -le 0 ]; then
-    echo "> No music to burn.";
-    echo "> Example: $0 *.mp3";
+    echo "Usage: $0 *.mp3";
     exit 255;
 fi
 
-
-### confirm
 for song in "$@"; do
     echo " $song";
 done
@@ -21,19 +16,15 @@ if [ "x$answer" != "xy" ]; then
     exit 0;
 fi
 
+echo "Burnprogress started. Please wait...";
 
-### burn mp3-files from the current folder
-echo "Burnprogress started!";
 for song in "$@"; do
-	mpg123 --cdr - "$song" |                                   \
-    cdrecord dev=/dev/hdc -v -audio -pad -nofix gracetime=2 -  \
-    || exit $?;
+    mpg123 --cdr - "$song" \
+        | cdrecord dev=/dev/hdc -v -audio -pad -nofix gracetime=2 - \
+        || exit $?;
 done
 
-
-### close disc
+# close disc
 cdrecord -fix || exit $?;
 
-
-### the end
 exit 0;
