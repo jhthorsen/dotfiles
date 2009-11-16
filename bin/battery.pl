7d06aa6eaf69727740627e71d5730d6c0d91c53e
Jan Henning Thorsen <jhthorsen@cpan.org>
2009-11-16 19:39:34 +0100

    rewrote battery.pl

--- bin/battery.pl
+++ bin/battery.pl
@@ -1,70 +1,84 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+battery.pl - Print battery status
+
+=head1 SYNOPSIS
+
+ battery.pl;
+ battery.pl /proc/acpi/battery/BAT0;
+
+=cut
+
 use strict;
 use warnings;
 
+my $base = shift || '/proc/acpi/battery/BAT0';
+my $batt_info = "$base/info";
+my $batt_stat = "$base/state";
+my %info = (read_proc($batt_info), read_proc($batt_stat));
+my @format = (
+    [ charging_state => 'Battery state' => '%s' ],
+    [ present_rate => 'Discharge rate' => '%5i mW' ],
+    [ remaining_capacity => 'Remaining power' => '%5i mWh' ],
+    [ time_left => 'Remaining time' => '%5.2f h' ],
+    [ percent_left => 'Remaining percentage' => '%5.2f %%' ],
+);
 
-### init
-my $batt_info = "/proc/acpi/battery/BAT0/info";
-my $batt_stat = "/proc/acpi/battery/BAT0/state";
-my %stat      = (read_proc($batt_info), read_proc($batt_stat));
-
-### print state
-printf "%-20s: %s\n",
-       "State",
-       ucfirst($stat{'charging_state'}),
-       ;
-
-### print rate
-printf "%-20s: %5.2f W\n",
-       "Rate",
-       $stat{'present_rate'} / 1e3,
-       ;
-
-### print remaining
-printf "%-20s: %5.2f Wh\n",
-       "Remaining capasity",
-       $stat{'remaining_capacity'} / 1e3,
-       ;
-
-### print rate
-if($stat{'present_rate'} > 0) {
-    my $time_left = $stat{'remaining_capacity'} / $stat{'present_rate'};
-    printf "%-20s: %ih %im (%0.1f%%)\n",
-           "Time left",
-           $time_left,
-           ($time_left - int($time_left)) * 60,
-           $stat{'remaining_capacity'} / $stat{'last_full_capacity'} * 100,
-           ;
-}
-else {
-    printf "%-20s: %s\n",
-           "Time left",
-           "Infinite",
-           ;
+for my $f (@format) {
+    printf "%-22s $f->[2]\n", $f->[1], $info{$f->[0]};
 }
 
-### the end
 exit 0;
 
+=head1 FUNCTIONS
 
-sub read_proc { #=============================================================
+=head2 read_proc
 
-    ### init
-    my $file = shift || "";
+ %info = read_proc($file);
+
+Returns information from a /proc file.
+
+=cut
+
+sub read_proc {
+    my $file = shift or return
     my %info;
 
-    open(my $fh, "<", $file) or die "Coult not open $file\n";
-    %info = map {
-                    my($k, $v) = /([^:]+):\s*(\w+)/;
-                    $k         =~ s/\s/_/g;
-                    $k         => $v;
-                } (@_ = <$fh>);
-    close $fh;
+    open(my $FH, "<", $file) or die "Coult not open $file\n";
 
-    ### the end
+    LINE:
+    while(<$FH>) {
+        chomp;
+        my($key, $value) = /(\w[^:]+):\s*(\w+)/ or next;
+        $key =~ s/\s/_/g;
+        $info{$key} = $value;
+    }
+
+    if(defined $info{'present_rate'}) {
+        if($info{'present_rate'} > 0) {
+            $info{'time_left'}
+                = $info{'remaining_capacity'} / $info{'present_rate'};
+            $info{'percent_left'}
+                = $info{'remaining_capacity'} / $info{'last_full_capacity'} * 100;
+        }
+        else {
+            $info{'time_left'} = -1;
+            $info{'percent_left'} = -1;
+        }
+    }
+
     return %info;
 }
 
-#=============================================================================
-__END__
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Henning Thorsen jhthorsen -at- cpan.org
+
+=cut
