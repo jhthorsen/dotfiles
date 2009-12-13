9f99a798163907fff03fe6e53b432a43da6f7cef
Jan Henning Thorsen <jhthorsen@cpan.org>
2009-12-13 18:39:34 +0100

    add (bad) example for transcoding

--- media/nrk-downloader.sh
+++ media/nrk-downloader.sh
@@ -3,7 +3,7 @@
 man() {
     cat <<'MAN'
 NAME
-    nrk-downloader.sh - Download streams from nrk.no - version 0.02
+    nrk-downloader.sh - Download streams from nrk.no - version 0.03
 
 SYNOPSIS
     Help:
@@ -33,6 +33,13 @@ MAN
 HREF=$1;
 COOKIE='Cookie: NetTV2.0Speed=NetTV2.0Speed=10000';
 
+VCODEC=${VCODEC-mp4v}
+VBITRATE=${VBITRATE-1024}
+ACODEC=${ACODEC-mp3}
+ARATE=${ARATE-128}
+ACHANNELS=${ACHANNELS-2}
+MUX=${MUX-asf}
+
 download () {
     param_value=$(
         wget --header "$COOKIE" "$1" -q -O- \
@@ -55,9 +62,16 @@ download () {
     fi
 
     echo "# Streaming $mms_file";
-    vlc $mms_file vlc://quit \
-        --sout file/avi:$out_file \
-        --quiet -I dummy &
+
+    if [ -z $TRANSCODE ]; then
+        cvlc $mms_file vlc://quit \
+            --sout file/avi:$out_file \
+            --quiet -I dummy &
+    else
+        cvlc $mms_file vlc://quit \
+            --sout "#transcode{vcodec=$VCODEC,vb=$VBITRATE,acodec=$ACODEC,ab=$ARATE,channels=$ACHANNELS}:standard{mux=$MUX,dst='$out_file',access=file}"
+            --quiet -I dummy &
+    fi
 
     VLC_PID=$!
 
