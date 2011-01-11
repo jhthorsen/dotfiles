bd75944ce922cd61f649d3e42ff1eb07648b56f9
Jan Henning Thorsen <jhthorsen@cpan.org>
2011-01-11 21:49:59 +0100

    add templates

---
+++ templates/Controller.pm
@@ -0,0 +1,39 @@
+package My::Module;
+
+=head1 NAME
+
+My::Module - Catalyst controller
+
+=head1 DESCRIPTION
+
+=cut
+
+use CatalystX::Controller::Sugar;
+
+=head1 ACTIONS
+
+=head2 Chain /
+
+=cut
+
+chain sub {
+};
+
+=head2 Enporint /
+
+=cut
+
+chain '' => sub {
+};
+
+=head1 BUGS
+
+=head1 COPYRIGHT & LICENSE
+
+=head1 AUTHOR
+
+See L<Top::Module>.
+
+=cut
+
+1;
