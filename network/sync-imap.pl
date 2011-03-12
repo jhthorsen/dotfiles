d1c4928a66e33a44bd58b38451fd1503d6741ceb
Jan Henning Thorsen <jhthorsen@cpan.org>
2011-03-12 19:50:48 +0100

    Add documentation and moved code around

--- network/sync-imap.pl
+++ network/sync-imap.pl
@@ -14,16 +14,82 @@ BEGIN {
 
 #=============================================================================
 package App::SyncIMAP::Client;
+
+=head1 NAME
+
+App::SyncIMAP::Client
+
+=head1 DESCRIPTION
+
+=head1 ENVIRONMENT
+
+=head2 VERBOSITY_SYNCIMAP
+
+This environment variable will set the verbosity level for this code.
+The default is "1", but it can be forced to "0" or "5".
+
+=over 4
+
+=item 0 means silence.
+
+=item 1 means print "useful" information to screen.
+
+=item 5 means print internal debugging.
+
+=back
+
+=cut
+
 use Carp;
 use Cwd;
 use Data::Dumper ();
 use File::Path qw/ make_path /;
 use Net::IMAP::Simple;
 use MIME::Base64 ();
+use constant VERBOSITY => defined $ENV{'VERBOSITY_SYNCIMAP'} ? $ENV{'VERBOSITY_SYNCIMAP'} : 1;
 use base 'Class::Accessor::Fast::WithBuilder';
 
 our $AUTOLOAD; # TODO: define fixed proxy methods instead
 our $MAIL_MAP = '.mail_map';
+
+=head1 ATTRIBUTES
+
+=head2 maildir
+
+Default "Maildir".
+
+=head2 port
+
+Default 993.
+
+=head2 server
+
+Default "localhost". Could be set to "imap.google.com" for Google.
+
+=head2 ssl
+
+Default 1 if L</port> is "993". Can be specified if L</port> is not
+the standard "993".
+
+=head2 username
+
+Required input. Plain string with the login username. Must contain
+the whole email address for some services, such as Google.
+
+=head2 password
+
+Required input. Should be a L<MIME::Base64> encoded string to make
+it a bit harder to read.
+
+=head2 timeout
+
+Default 10. Given in seconds.
+
+=head2 trash_mailbox
+
+Default "Trash". Could be set to "[Gmail]/Trash" for Google.
+
+=cut
 
 __PACKAGE__->mk_accessors(qw(
     _client
@@ -44,20 +110,52 @@ sub _build_trash_mailbox { 'Trash' }
 sub _build_timeout { 10 }
 sub _build_server { 'localhost' }
 sub _build_ssl { shift->port eq '993' ? 1 : 0 }
-sub _build_username { shift->throw_exception('Usage: $self->new({ username => $Str, ... })') }
-sub _build_password { shift->throw_exception('Usage: $self->new({ password => $Str, ... })') }
+sub _build_username { shift->_throw_exception('Usage: $self->new({ username => $Str, ... })') }
+sub _build_password { shift->_throw_exception('Usage: $self->new({ password => $Str, ... })') }
 
 sub _build__mail_map {
     my $self = shift;
     open my $MAP_FH, '<', $self->maildir .'/' .$MAIL_MAP or return {};
     local $/; # slurp
-    my $mail_map = eval(readline $MAP_FH) or $self->throw_exception($@);
+    my $mail_map = eval(readline $MAP_FH) or $self->_throw_exception($@);
     return $mail_map;
 }
 
+=head1 METHODS
+
+=head2 track_file
+
+    $self->track_file($file, $timestamp);
+
+Will mark a file as tracked.
+
+=head2 tracked_files
+
+    @filenames = $self->tracked_files;
+
+Returns a list of files relative to L</maildir>.
+
+=head2 tracked_file_timestamp
+
+    $int = $self->tracked_file_timestamp($file);
+
+Will return an epoch time for the last time the file was tracked.
+The timestamp is set inside L</sync>. Returns C<undef> if the file
+is not tracked.
+
+=cut
+
 sub track_file { $_[0]->_mail_map->{'file_to_timestamp'}{$_[1]} = $_[2] }
 sub tracked_files { keys %{ $_[0]->_mail_map->{'file_to_timestamp'} } }
-sub tracked_file { $_[0]->_mail_map->{'file_to_timestamp'}{$_[1]} || 0 }
+sub tracked_file_timestamp { $_[0]->_mail_map->{'file_to_timestamp'}{$_[1]} || 0 }
+
+=head2 untrack_file
+
+    $self->untrack_file($file);
+
+Will remove the tracking of the file in the internal storage.
+
+=cut
 
 sub untrack_file {
     my($self, $file) = @_;
@@ -66,6 +164,17 @@ sub untrack_file {
     delete $self->_mail_map->{'file_to_timestamp'}{$file};
     delete $self->_mail_map->{'uid_to_file'}{$uid} if($uid);
 }
+
+=head2 uid_to_file
+
+    $file = $self->uid_to_file($uid, $file);
+    $file = $self->uid_to_file($uid);
+
+The first will store the L<uid|Net::IMAP::Simple/uid> for a given file
+and the second will simply return the uid if it exists. Returns
+C<undef> if the uid is not mapped to a file.
+
+=cut
 
 sub uid_to_file {
     my $self = shift;
@@ -89,59 +198,108 @@ sub _build__client {
         retry => 1,
         retry_delay => 5,
         use_v6 => 0,
-        debug => $ENV{'SYNCAPP_DEBUG'} ? 'warn' : undef,
+        debug => VERBOSITY == 5 ? 'warn' : undef,
         #use_select_cache => 1,
         #select_cache_ttl => 60,
     );
 }
 
+=head2 login
+
+Will use L</username> and L</password> to login. Will throw an exception
+if it fails.
+
+=cut
+
 sub login {
-    return $_[0]->_client->login(
-        $_[0]->username,
-        MIME::Base64::decode_base64($_[0]->password),
-    );
-}
-
-sub dump {
     my $self = shift;
+    my $password = MIME::Base64::decodeselfe64($self->password);
 
-    for my $method (qw/ uidnext examine flags current_box /) {
-        printf "%s: %s\n", $method, join ',', $self->$method;
+    unless($self->_client->login($self->username, $password)) {
+        $self->_throw_exception;
     }
-
-    for my $box ($self->mailboxes) {
-        printf "mailbox: /%s\n", $box;
-    }
-    #for my $box ($self->mailboxes_subscribed) {
-    #    printf "mailbox subscribed: /%s\n", $box;
-    #}
 
     return 1;
 }
 
+=head2 dump
+
+    ??? = $self->dump;
+
+TBD.
+
+=cut
+
+sub dump {
+    my $self = shift;
+    my %res;
+
+    for my $method (qw/ uidnext examine flags current_box /) {
+        $res{$method} = [$self->$method];
+        printf "%s: %s\n", $method, join ',', @{ $res{$method} } if VERBOSITY;
+    }
+
+    for my $box ($self->mailboxes) {
+        push @{ $res{'mailboxes'} }, $box;
+        printf "mailbox: /%s\n", $box if VERBOSITY;
+    }
+    #for my $box ($self->mailboxes_subscribed) {
+    #    printf "mailbox subscribed: /%s\n", $box if VERBOSITY;
+    #}
+
+    return \%res;
+}
+
+=head2 sync
+
+Will sync the given L</maildir> to/from the given L</server>. It will
+create the directories and mails on local disk when seen on the server:
+
+    $maildir/$mailbox/$uid
+
+The C<$mailbox> part may contain more than one level of directories.
+The C<$uid> is used to track which files was seen and not. In addition
+it will store the UID and file information in
+
+    $maildir/.mail_map
+
+which is a file containg a complex Perl data structure.
+
+This method use L<Net::IMAP::Simple/mailboxes> to retrieve the mailbox
+list, L</sync_message> to do the actual syncing and last
+L<Net::IMAP::Simple/expunge_mailbox> to expunge any mailes marked for
+deletion. After the chatting with the server it will delete all the
+L<files|/tracked_files> on disk which is not on the server.
+
+=cut
+
 sub sync {
     my $self = shift;
     my $time = time;
-    my $old_dir = cwd or $self->throw_exception('Failed to get current directory');
+    my $old_dir = cwd or $self->_throw_exception('Failed to get current directory');
 
-    print "Syncing ", $self->maildir, " ...\n";
-    chdir $self->maildir or $self->throw_exception('Maildir does not exist:', $self->maildir);
+    print "Syncing ", $self->maildir, " ...\n" if VERBOSITY;
+    chdir $self->maildir or $self->_throw_exception('Maildir does not exist:', $self->maildir);
 
-    MAILBOX:
     for my $box ($self->mailboxes) {
+        my $n_messages = $self->select($box) or next;
+
         unless(-d $box) {
-            print "> make_path '$box'\n";
+            print "> make_path '$box'\n" if VERBOSITY;
             make_path $box;
         }
 
-        $self->sync_messages($box, $time);
-        $self->expunge_mailbox($box);
+        for my $message_number (1..$n_messages) {
+            $self->sync_message($message_number, $time);
+        }
+
+        $self->expunge_mailbox;
     }
 
     FILE:
     for my $file ($self->tracked_files) {
-        if($self->tracked_file($file) < $time) {
-            print "> deleted on server: $file\n";
+        if($self->tracked_file_timestamp($file) < $time) {
+            print "> deleted on server: $file\n" if VERBOSITY;
             unlink $file;
             $self->untrack_file($file);
         }
@@ -151,50 +309,102 @@ sub sync {
     return $self->write_mail_map;
 }
 
-# need to be called after chdir
-sub sync_messages {
-    my($self, $box, $time) = @_;
-    my $trash_mailbox = $self->trash_mailbox;
-    my $n_messages = $self->select($box) or return 0;
+=head2 sync_message
 
-    MESSAGE:
-    for my $message_number (1..$n_messages) {
-        my($uid) = $self->uid($message_number) or next MESSAGE;
-        my $source = $self->uid_to_file($uid);
-        my $file = "$box/$uid";
+    $self->sync_message($message_number, $timestamp);
 
-        if(-e $file) {
-            $self->track_file($file, $time);
-            $self->uid_to_file($uid, $file);
-        }
-        elsif($self->tracked_file($file)) {
-            if($trash_mailbox eq $box) {
-                printf "> expunge from %s: %s\n", $box, $file;
-            }
-            else {
-                printf "> move to %s: %s\n", $trash_mailbox, $file;
-                $self->copy($message_number, $trash_mailbox) or $self->throw_exception;
-            }
-            $self->delete($message_number) or $self->throw_exception;
-            $self->untrack_file($file);
-        }
-        elsif($source and -r $source and $source eq $file) {
-            $source =~ s/^\.\.\///;
-            print "> link $source => $file\n";
-            link $source => $file or $self->throw_exception("link '$source' => '$file' failed: $!");
-        }
-        else {
-            print "> download $file\n";
-            my $IMAP_MAIL = $self->getfh($message_number) or $self->throw_exception;
-            open my $LOCAL_MAIL, '>', $file or $self->throw_exception("Write $file: $!");
-            print $LOCAL_MAIL $_ while(<$IMAP_MAIL>);
-            $self->track_file($file, $time);
-            $self->uid_to_file($uid, $file);
-        }
+This method need to be called after called after C<chdir>ed to
+the L</maildir>. It will do:
+
+=over 4
+
+=item *
+
+Track and skip the file if it already exists on the filesystem
+
+=item *
+
+Delete the file from server if the file was previously tracked,
+but no longer exists on the filesystem.
+
+=item *
+
+Download the file from server and store it on disk.
+
+=back
+
+=cut
+
+sub sync_message {
+    my($self, $message_number, $time) = @_;
+    my($uid) = $self->uid($message_number) or $self->_throw_exception;
+    my $current_box = $self->current_box;
+    my $source = $self->uid_to_file($uid);
+    my $file = "$current_box/$uid";
+
+    if(-e $file) {
+        $self->track_file($file, $time);
+        $self->uid_to_file($uid, $file);
+    }
+    elsif($self->tracked_file_timestamp($file)) {
+        $self->remote_delete($message_number, $file);
+    }
+# TODO: How can this work? Looks like at least google mess up UID
+# when making drafts...
+#    elsif($source and -r $source) {
+#        $source =~ s/^\.\.\///;
+#        print "> link $source => $file\n" if VERBOSITY;
+#        link $source => $file or $self->_throw_exception("link '$source' => '$file' failed: $!");
+#    }
+    else {
+        print "> download $file\n" if VERBOSITY;
+        my $IMAP_MAIL = $self->getfh($message_number) or $self->_throw_exception;
+        open my $LOCAL_MAIL, '>', $file or $self->_throw_exception("Write $file: $!");
+        print $LOCAL_MAIL $_ while(<$IMAP_MAIL>);
+        $self->track_file($file, $time);
+        $self->uid_to_file($uid, $file);
     }
 
-    return $n_messages;
+    return 1;
 }
+
+=head2 remote_delete
+
+    $self->remote_delete($message_number, $file);
+
+Will delete an email on the server with the given C<$message_number> and
+in the L<current mailbox|Net::IMAP::Simple/current_box>.
+
+The email will be copied to trash first and the later expunge it if current
+mailbox is l</trash_mailbox>.
+
+=cut
+
+sub remote_delete {
+    my($self, $message_number, $file) = @_;
+    my $current_box = $self->current_box;
+    my $trash_mailbox = $self->trash_mailbox;
+
+    if($trash_mailbox eq $current_box) {
+        printf "> expunge from %s: %s\n", $current_box, $file if VERBOSITY;
+    }
+    else {
+        printf "> move to %s: %s\n", $trash_mailbox, $file if VERBOSITY;
+        $self->copy($message_number, $trash_mailbox) or $self->_throw_exception;
+    }
+
+    $self->delete($message_number) or $self->_throw_exception;
+    $self->untrack_file($file);
+
+    return 1;
+}
+
+=head2 write_mail_map
+
+Will dump the tacked UID and file information to disk. This method
+is the last thing L</sync> calls.
+
+=cut
 
 sub write_mail_map {
     my $self = shift;
@@ -205,12 +415,20 @@ sub write_mail_map {
     print $MAP_FH Data::Dumper::Dumper($self->_mail_map);
 }
 
-sub throw_exception {
+sub _throw_exception {
     my($self, @msg) = @_;
     @msg = ($self->errstr) unless(@msg);
     s/[\r\n]//g for(@msg);
     Carp::confess(join ' ', @msg);
 }
+
+=head2 new
+
+    $self = $class->new(\%attr);
+
+Object constructor. See L</ATTRIBUTES> for details on constructor arguments.
+
+=cut
 
 sub new {
     my $self = shift->SUPER::new(@_);
@@ -221,6 +439,13 @@ sub new {
     
     return $self;
 }
+
+=head1 AUTOLOAD
+
+Yes... I know C<AUTOLOAD> is evil. The rest of the methods available
+on this object is proxied to L<Net::IMAP::Simple>.
+
+=cut
 
 sub AUTOLOAD {
     my $method = ($AUTOLOAD =~ /::(\w+)$/)[0];
