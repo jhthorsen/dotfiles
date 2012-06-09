71d62dc147d0cbcbd1ac813400925ee88997f5c3
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-06-09 16:17:25 +0200

    Will automatically find the new version of spotify

--- media/spotify-apps-installer.sh
+++ media/spotify-apps-installer.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 WRAPPER_BIN="/usr/bin/spotify";
-SPOTIFY_DEB="http://download.spotify.com/preview/spotify-client_0.8.2.572.geb65f9a.433-1_amd64.deb";
+POOL="http://repository.spotify.com/pool/non-free/s/spotify/";
 SPOTIFY_PLUGIN_PATH="/opt/spotify/plugin";
+DOWNLOAD_DIR="/tmp";
 FLASH_URL="http://download.macromedia.com/pub/labs/flashplayer10/flashplayer10_2_p3_64bit_linux_111710.tar.gz";
-TMP="/tmp/spotify-installer.tmp";
+SKIP=$([ $(uname -p) = "x86_64" ] && echo "i386" || echo "amd64");
 SELF="$(cd -P $(dirname $0); pwd)/$(basename $0)";
 
 function download() {
-    URL=$1;
+    URL="$1";
+    TARGET="/tmp/$2";
     TITLE=$(basename $URL);
 
     echo "wget $URL";
-    wget $URL -O $TMP 2>&1 \
+    wget $URL -O $TARGET 2>&1 \
         | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' \
         | zenity --progress --text="Downloading $TITLE" --title="Downloading $TITLE ..." --auto-close --auto-kill
 
@@ -26,7 +28,7 @@ if [ "$USER" == "root" ]; then
     fi
     if [ -z "$SKIP_DOWNLOAD" ]; then
         apt-get install libqt4-webkit;
-        dpkg -i $TMP;
+        dpkg -i $DOWNLOAD_DIR/spotify*deb;
     fi
 
     TYPE=$(file -b --mime-type $WRAPPER_BIN);
@@ -44,13 +46,26 @@ CODE
     ) > $WRAPPER_BIN;
     chmod +x $WRAPPER_BIN;
 
-else
+else # $USER is not "root"
     zenity --question --text "Do you want to install spotify with app support?" --title "Install spotify?" || exit $1;
     if [ ! -e "$SPOTIFY_PLUGIN_PATH/libflashplayer.so" ]; then
-        download $FLASH_URL;
-        tar xfzv $TMP;
+        download $FLASH_URL $(basename $FLASH_URL);
+        tar xfzv $DOWNLOAD_DIR/$(basename $FLASH_URL);
     fi
-    [ -z "$SKIP_DOWNLOAD" ] && download $SPOTIFY_DEB;
+    if [ -z "$SKIP_DOWNLOAD" ]; then
+        PACKAGES=0;
+        for p in $(wget -q $POOL -O - | grep -o -E 'href="([^"#]+)"' | cut -d'"' -f2 | uniq); do
+            PACKAGES=$((PACKAGES+1));
+            [ "x$VERBOSE" != "x" ] && echo "Found package $p in deb pool";
+            echo $p | grep "^spotify" >/dev/null || continue;
+            [ -e "$p" ] && continue;
+            echo $p | grep $SKIP > /dev/null || download "$POOL/$p" $p;
+        done
+        if [ "x$PACKAGES" = "x0" ]; then
+            zenity --info --text "Could not get package list from $POOL";
+        fi
+    fi
+
     gksudo -k $SELF;
     zenity --info --text "Spotify was installed. It can be started from your application menu";
 fi
