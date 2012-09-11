938dce36e2df2269b9ab1bf6db9c3495c35d5c8f
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-09-11 21:32:21 +0200

    stream-desktop.sh can output rtsp (?)

--- media/stream-desktop.sh
+++ media/stream-desktop.sh
@@ -1,10 +1,44 @@
 #!/bin/sh
 # This script is used to capture the screen and save it to a file
+
+#==============================
+# Install deps
 # sudo apt-get install ffmpeg libavcodec-extra-53
 
-FILE="$1"
+#==============================
+# Install and start RTSP server
+# cpanm -n --sudo RTSP::Server
+# wget https://raw.github.com/revmischa/rtsp-server/master/rtsp-server.pl -O /usr/local/bin/rtsp-server.pl
+# sudo chmod +x /usr/local/bin/rtsp-server.pl
+# sudo rtsp-server.pl
+
+#=============================
+# Start capture
+# ./media/stream-desktop.sh
+
+#==============================
+# Join capture
+# mplayer rtsp://localhost/desktop
+
+#==============================
+# Example markup for flowplayer
+# <!DOCTYPE>
+# <html>
+# <head>
+#   <script src="/js/flowplayer.min.js"></script>
+#   <script type="text/javascript">
+#     window.onload = function() { flowplayer("player", "/flowplayer.swf") };
+#   </script>
+# </head>
+# <body>
+#   <a href="rtsp://localhost/desktop" id="player" style="display:block;width:800;height:600"></a>
+# </body>
+# </html>
+
 INRES="1280x800"
 OUTRES="1280x800"
 FPS="20"
 
-avconv -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -vcodec libx264 -s "$OUTRES" "$FILE"
+avconv \
+    -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -vcodec libx264 -s "$OUTRES" \
+    -f rtsp -muxdelay 0.1 rtsp://localhost:5545/desktop
