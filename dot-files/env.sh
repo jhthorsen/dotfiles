769cb247799593590aafea31b979dabef2b4538f
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-02-26 21:20:05 +0900

    Add env.sh and setkeylabel.sh

---
+++ dot-files/env.sh
@@ -0,0 +1,8 @@
+export ANSIBLE_NOCOWS=1
+export BROWSER=google-chrome
+export EDITOR=vim
+export GPG_TTY=$(tty)
+export LC_ALL=en_US.UTF-8
+export LESS="-RX -x2"
+export SSH_KEY_PATH="~/.ssh/id_rsa"
+export TT_HOURS_PER_DAY=6
--- dot-files/install.sh
+++ dot-files/install.sh
@@ -39,6 +39,8 @@ install_file $ROOT_DIR/.vimrc $HOME/.vimrc
 install_file $ROOT_DIR/.zshrc $HOME/.zshrc
 
 # .zshrc dependencies
-install_file $ROOT_DIR/path.sh $CONFIG/00-path.sh
-install_file $ROOT_DIR/aliases.sh $CONFIG/10-aliases.sh
-install_file $ROOT_DIR/history.sh $CONFIG/10-history.sh
+install_file $ROOT_DIR/path.sh $CONFIG_DIR/00-path.sh
+install_file $ROOT_DIR/env.sh $CONFIG_DIR/01-env.sh
+install_file $ROOT_DIR/aliases.sh $CONFIG_DIR/10-aliases.sh
+install_file $ROOT_DIR/history.sh $CONFIG_DIR/10-history.sh
+install_file $ROOT_DIR/setkeylabel.sh $CONFIG_DIR/30-setkeylabel.sh
