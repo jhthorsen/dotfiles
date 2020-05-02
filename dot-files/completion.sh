a7a55a3fca7b415cd68b792207312584ca50b4a0
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-05-02 09:30:22 +0900

    Add "reload" alias and shift+tab keybinding

--- dot-files/completion.sh
+++ dot-files/completion.sh
@@ -1,6 +1,9 @@
+bindkey '^[[Z' reverse-menu-complete
+
 setopt auto_list # automatically list choices on ambiguous completion
 setopt auto_menu # automatically use menu completion
 unsetopt always_to_end
+
 zstyle ':completion:*' menu select # select completions with arrow keys
 zstyle ':completion:*' group-name '' # group results by category
 zstyle ':completion:::::' completer _expand _complete _ignored _approximate # enable approximate matches for completion
