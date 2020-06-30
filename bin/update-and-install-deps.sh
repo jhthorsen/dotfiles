0f64858eb0b46c8fb28c72b675083aba8a74df4c
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-07-01 07:56:08 +0900

    Add bin/update-and-install-deps.sh

---
+++ bin/update-and-install-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ROOT_DIR=$(pwd);
+
+for i in *; do
+  if [ -d "$i/.git" ]; then
+    echo "";
+    echo "--- $i";
+
+    cd $i;
+    git remote update;
+    git pull;
+    [ -e "cpanfile" -o -e "Makefile.PL" ] && cpanm -n --installdeps .
+  fi
+
+  cd $ROOT_DIR;
+done
