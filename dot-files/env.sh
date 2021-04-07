0fe6e4f4f91f2e397437952f8e7ae395f855a480
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-04-07 09:47:17 +0900

    Using gruvbox colors in terminal

--- dot-files/env.sh
+++ dot-files/env.sh
@@ -8,6 +8,7 @@ export TT_HOURS_PER_MONTH=150
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
+[ -f "$HOME/.vim/plugged/gruvbox/gruvbox_256palette_osx.sh" ] && source "$HOME/.vim/plugged/gruvbox/gruvbox_256palette_osx.sh";
 [ -x "/usr/bin/nvim" ] && export VIM_BIN="/usr/bin/nvim"
 [ -x "/usr/local/bin/nvim" ] && export VIM_BIN="/usr/local/bin/nvim"
 
