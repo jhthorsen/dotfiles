8717ac9250bb49d1073c9f8e45c67e4ca92257d1
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-10-25 10:41:18 +0900

    Add helper for installing modules on macos

---
+++ perl/macos-install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [ -n "$1" ]; then
+  MODULE=$1;
+  echo "\$ cpanm $MODULE";
+  cpanm $MODULE && exit 0;
+
+  echo "# cpanm $MODULE FAILED $?";
+  WORK_ID=$(ls -r $HOME/.cpanm/work | head -n1);
+  MODULE_DIR=$(echo $MODULE | perl -pe's!::!-!g');
+  cd $HOME/.cpanm/work/$WORK_ID/$MODULE_DIR-* || exit 1;
+  $0;
+  exit $?;
+fi
+
+TARGET=$(basename $PWD);
+
+if echo $TARGET | grep -q "Crypt-OpenSSL-Bignum"; then
+  OPENSSL_INCLUDE="-I/usr/local/opt/openssl/include" \
+    OPENSSL_LIB="-L/usr/local/opt/openssl/lib" \
+    perl Makefile.PL || exit $?
+  make && make install;
+
+elif echo $TARGET | grep -q "DBD-MariaDB"; then
+  perl Makefile.PL \
+    --libs="-L/usr/local/opt/openssl/lib -L/usr/local/opt/mysql-client/lib -lmysqlclient" \
+    --testuser=root || exit $?;
+  make && make install
+
+elif echo $TARGET | grep -q "DBD-mysql"; then
+  perl Makefile.PL \
+    --libs="-L/usr/local/opt/openssl/lib -L/usr/local/opt/mysql-client/lib -lmysqlclient" \
+    --testuser=root || exit $?;
+  make && make install
+
+else
+  echo "Usage";
+  echo "$0 Crypt::OpenSSL::Bignum";
+  echo "$0 DBD::mysql";
+fi
+
