17677db1121e528f7ca6ba0f2dffbabd4ab35ae0
Jan Henning Thorsen <jan.henning@thorsen.pm>
2022-04-23 12:37:26 +0900

    Improved clipboard handling and fixed ^C bug

--- config/nvim/lua/config/clipboard.lua
+++ config/nvim/lua/config/clipboard.lua
@@ -1,6 +1,11 @@
 local utils = require('../utils')
+local set_keymap = vim.api.nvim_set_keymap
 
 vim.o.clipboard = 'unnamed'
-vim.api.nvim_set_keymap('v', '<c-v>', ':w !snipclip -i<CR><CR>', {})
-vim.api.nvim_set_keymap('v', '<c-v>', 'c<ESC>:set paste<CR>:r !snipclip -o<CR>:set nopaste<CR>', {})
-vim.api.nvim_set_keymap('i', '<c-v>', '<ESC>:set paste<CR>:r !snipclip -o<CR>:set nopaste<CR>a', {})
+utils.autocmd('clipboard', {{'TextYankPost', 'if v:event.operator ==# "y" | call system("snipclip -i", @0) | endif'}});
+set_keymap('v', '<c-c>', ':w !snipclip -i<CR><CR>', {})
+set_keymap('i', '<c-v>', '<ESC>:set paste<CR>:r !snipclip -o<CR>:set nopaste<CR>a', {})
+
+-- 0dd, 0de, ... does not cut - it just deletes
+set_keymap('n', '0d', '"_d', {noremap = true})
+set_keymap('v', '0d', '"_d', {noremap = true})
