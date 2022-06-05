88929b909ad19c153b9fbe7e3cd93ef524df99c0
Jan Henning Thorsen <jan.henning@thorsen.pm>
2022-06-05 15:41:55 +0900

    Improved diagnostics popup

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -1,17 +1,22 @@
 -- vim.lsp.set_log_level('debug')
 local utils = require('../utils')
-utils.autocmd('filetypes', {{'BufRead,BufNewFile', '*.css', 'set filetype=scss'}})
+utils.autocmd('lsp', {{'BufRead,BufNewFile', '*.css', 'set filetype=scss'}})
+
+utils.autocmd('lsp', {{'CursorHold,CursorHoldI', '*', 'lua vim.diagnostic.open_float(nil, {focus=false})'}})
+vim.diagnostic.config({virtual_text = false})
 
 local function on_attach(client, bufnr)
   vim.o.updatetime = 300
   vim.o.omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-  local opts = {noremap = true, silent = true}
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  local opts = {buffer = bufnr, noremap = true, silent = true}
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
