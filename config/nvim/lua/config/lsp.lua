69e4f47928696b327be6563ab90bbceace331359
Jan Henning Thorsen <jan.henning@thorsen.pm>
2022-07-05 10:36:28 +0200

    Using https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/volar.lua

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -22,11 +22,17 @@ end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 
+local volar_init_options = {
+  typescript = {
+    serverPath = '/opt/homebrew/lib/node_modules/typescript/lib/tsserverlibrary.js'
+  }
+}
+
 lspconfig.bashls.setup({capabilities = capabilities, on_attach = on_attach})
 lspconfig.cssls.setup({capabilities = capabilities, on_attach = on_attach})
 lspconfig.html.setup({capabilities = capabilities, on_attach = on_attach})
 lspconfig.perlpls.setup({capabilities = capabilities, on_attach = on_attach});
 lspconfig.svelte.setup({capabilities = capabilities, on_attach = on_attach})
 lspconfig.tsserver.setup({capabilities = capabilities, on_attach = on_attach})
-lspconfig.vuels.setup({capabilities = capabilities, on_attach = on_attach})
+lspconfig.volar.setup({capabilities = capabilities, on_attach = on_attach, init_options = volar_init_options})
 lspconfig.yamlls.setup({capabilities = capabilities, on_attach = on_attach})
