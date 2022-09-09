eb33f4df6a5e80234a8565301dc937fc7f81e114
Jan Henning Thorsen <jan.henning@thorsen.pm>
2022-09-09 13:33:49 +0900

    Have to run "git ship" twice to actually push/upload anything

--- templates/perl/git-ship.sh
+++ templates/perl/git-ship.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+project_is_ready() {
+  grep 'Not Released' ./Changes && return 1;
+  test -n "$(find ./ -maxdepth 1 -name "*.tar.gz")" || return 1;
+  return 0;
+}
+
 prepare_project() {
   MAIN_PM_FILE="$(grep ABSTRACT_FROM Makefile.PL | cut -d\' -f 2 | head -n1)";
   NEXT_VERSION="$(grep 'Not Released' ./Changes | head -n1 | awk '{print $1}')";
@@ -14,6 +20,11 @@ build_project() {
   run perl Makefile.PL;
   run make all;
   run make manifest;
+  run make dist;
+  run git add Changes Makefile.PL README.md lib/;
+  git_commit_maybe "Released $NEXT_VERSION";
+  git show -s "refs/tags/v$NEXT_VERSION" &>/dev/null && run git tag -d "v$NEXT_VERSION";
+  run git tag "v$NEXT_VERSION";
 }
 
 test_project() {
@@ -21,14 +32,9 @@ test_project() {
 }
 
 ship_project() {
-  run git add Changes Makefile.PL README.md lib/;
-  git_commit_maybe "Released $NEXT_VERSION";
-  git show -s "refs/tags/v$NEXT_VERSION" &>/dev/null && run git tag -d "v$NEXT_VERSION";
-  run git tag "v$NEXT_VERSION";
-  [ -z "$OFFLINE" ] && run git push origin "$(git branch --show-current)";
-  [ -z "$OFFLINE" ] && run git push origin --tags;
-  run make dist;
-  [ -z "$OFFLINE" ] && run cpan-upload ./*.tar.gz;
+  run git push origin "$(git branch --show-current)";
+  run git push origin --tags;
+  run cpan-upload ./*.tar.gz;
   run make clean;
   run git clean -f -d;
 }
