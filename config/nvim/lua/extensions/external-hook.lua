9598715ef1b822b55fab9771a91d2b9e7c2de6f6
Jan Henning Thorsen <jan.henning@thorsen.pm>
2022-11-30 08:14:05 +0900

    Add ExternalHook user command

--- config/nvim/lua/extensions/external-hook.lua
+++ config/nvim/lua/extensions/external-hook.lua
@@ -1,36 +1,34 @@
 local hook_script = nil
 local vim_leave = false
 
-local hook = function(hook_name)
-  local file = vim.api.nvim_buf_get_name(0)
-
-  if hook_script == nil then
-    local dir = file
-    while #dir > 1 do
-      dir = vim.fs.dirname(dir)
-      local file = dir .. '/.nvim-external-hook'
-      local stat = vim.loop.fs_stat(file)
-      if stat then
-        hook_script = file
-        break
-      end
-    end
+local find_hook_script = function(path)
+  while #path > 1 do
+    path = vim.fs.dirname(path)
+    local script = path .. '/.nvim-external-hook'
+    local stat = vim.loop.fs_stat(script)
+    if stat then return script end
   end
 
-  if hook_script ~= nil then
-    local handle = vim.loop.spawn(
-      hook_script,
-      {args = {hook_name, file}},
-      function(code, signal)
-        if code ~= 2 and not vim_leave then
-          print(hook_name .. ' ' .. file .. ' = ' .. code)
-        end
-      end
-    )
-  end
+  return ''
 end
 
-vim.api.nvim_create_autocmd('BufNewFile', {callback = function() hook('BufNewFile') end})
-vim.api.nvim_create_autocmd('BufReadPost', {callback = function() hook('BufReadPost') end})
-vim.api.nvim_create_autocmd('BufWritePost', {callback = function() hook('BufWritePost') end})
+local hook = function(params)
+  local file = vim.api.nvim_buf_get_name(0)
+  if hook_script == nil then hook_script = find_hook_script(file) end
+  if hook_script == '' then return end
+
+  local event_name = params.args and params.args or params.event
+  local handle = vim.loop.spawn(
+    hook_script,
+    {args = {event_name, file}},
+    function(code, signal)
+      if code ~= 2 and not vim_leave then
+        print(event_name .. ' ' .. file .. ' = ' .. code)
+      end
+    end
+  )
+end
+
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufReadPost', 'BufWritePost'}, {callback = hook})
 vim.api.nvim_create_autocmd('VimLeave', {callback = function() vim_leave = true end})
+vim.api.nvim_create_user_command('ExternalHook', hook, {nargs = 1})
