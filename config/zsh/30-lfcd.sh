3402a2e4f466476877c41f1c52cbfd151e3ed166
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-09-04 07:45:00 +0900

    Fix indentation

--- config/zsh/30-lfcd.sh
+++ config/zsh/30-lfcd.sh
@@ -3,27 +3,27 @@
 # You need to either copy the content of this file to your shell rc file
 # (e.g. ~/.bashrc) or source this file directly:
 #
-#     LFCD="/path/to/lfcd.sh"
-#     if [ -f "$LFCD" ]; then
-#         source "$LFCD"
-#     fi
+#   LFCD="/path/to/lfcd.sh"
+#   if [ -f "$LFCD" ]; then
+#     source "$LFCD"
+#   fi
 #
 # You may also like to assign a key to this command:
 #
-#     bind '"\C-o":"lfcd\C-m"'  # bash
-#     bindkey -s '^o' 'lfcd\n'  # zsh
+#   bind '"\C-o":"lfcd\C-m"'  # bash
+#   bindkey -s '^o' 'lfcd\n'  # zsh
 #
 
 lfcd () {
-    tmp="$(mktemp)"
-    lf -last-dir-path="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp"
-        if [ -d "$dir" ]; then
-            if [ "$dir" != "$(pwd)" ]; then
-                cd "$dir"
-            fi
-        fi
+  tmp="$(mktemp)"
+  lf -last-dir-path="$tmp" "$@"
+  if [ -f "$tmp" ]; then
+    dir="$(cat "$tmp")"
+    rm -f "$tmp"
+    if [ -d "$dir" ]; then
+      if [ "$dir" != "$(pwd)" ]; then
+        cd "$dir" || exit $?;
+      fi
     fi
+  fi
 }
