85e43fbbcf07471df4af6338aa1bd6c4c258376f
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-09-27 11:30:26 +0900

    Trying to simplify wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,18 +2,18 @@ local wezterm = require 'wezterm'
 local keymap = require 'keymap'
 local key_table_copy_mode = require 'key_table_copy_mode'
 local key_table_search_mode = require 'key_table_search_mode'
-local launch_menu = require 'private_launch_menu'
+-- local launch_menu = require 'private_launch_menu'
 local mouse_bindings = require 'mouse_bindings'
-local ssh_domains = require 'ssh_domains'
+-- local ssh_domains = require 'ssh_domains'
 local theme_kanagawa = require 'theme_kanagawa'
 
 return {
-  launch_menu = launch_menu,
-  ssh_domains = ssh_domains,
+  -- launch_menu = launch_menu,
+  -- ssh_domains = ssh_domains,
   unix_domains = {
     {
       name = 'unix',
-      local_echo_threshold_ms = 1000,
+      -- local_echo_threshold_ms = 1000,
     },
   },
 
@@ -23,17 +23,17 @@ return {
   -- send_composed_key_when_right_alt_is_pressed = true,
 
   colors = theme_kanagawa,
-  force_reverse_video_cursor = true,
+  -- force_reverse_video_cursor = true,
   font = wezterm.font_with_fallback {
     {family = 'Hack'},
     {family = 'JetBrains Mono'},
   },
-  harfbuzz_features = {'calt=0', 'clig=1', 'liga=1'},
+  -- harfbuzz_features = {'calt=0', 'clig=1', 'liga=1'},
   font_size = 12,
   line_height = 1.3,
-  window_background_opacity = 0.98,
+  window_background_opacity = 0.96,
 
-  adjust_window_size_when_changing_font_size = true,
+  -- adjust_window_size_when_changing_font_size = true,
   audible_bell = 'Disabled',
   automatically_reload_config = true,
   check_for_updates = false,
@@ -50,10 +50,5 @@ return {
   enable_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
   native_macos_fullscreen_mode = false,
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
+  window_padding = {top = 3, right = 3, bottom = 3, left = 3},
 }
