98b85a8440230ff80a502e2298f042761ff5beb4
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-10-16 08:52:52 +0900

    Made wezterm optional in install.sh and made the script output more

--- install.sh
+++ install.sh
@@ -1,70 +1,71 @@
 #!/bin/zsh
 
-abort() {
-  echo "# ERROR $*" >&2;
-  exit 1;
-}
-
-lnk() {
-  FROM="$(readlink_f $1)";
-  TO="$2";
-  [ ! -e $1 ] && abort "$1 cannot be found";
-  [ "x$IMPORT" = "x1" -a ! -L $TO ] && run cp $TO $FROM;
-  [ -L $TO -a ! -r $TO ] && run rm $TO; # Remove broken links
-  [ -L $TO ] || run ln -s $FROM $TO;
-}
-
-readlink_f() {
-  [ "x$UNAME" = "xDarwin" ] && readlink $* || readlink -f $*;
-}
-
-run() {
-  echo "> $*" >&2;
-  [ "x$DRY_RUN" = "x" ] && $*;
-}
+XDG_CONFIG_DIR="$HOME/.config";
+XDG_DATA_HOME="$HOME/.local/share";
 
 install_misc() {
-  lnk config/ackrc $HOME/.ackrc;
-  lnk config/git $XDG_CONFIG_DIR/git;
-  lnk config/lf $XDG_CONFIG_DIR/lf;
+  lnk "config/ackrc" "$HOME/.ackrc";
+  lnk "config/git" "$XDG_CONFIG_DIR/git";
+  lnk "config/lf" "$XDG_CONFIG_DIR/lf";
 }
 
 install_tmux() {
-  lnk config/tmux/tmux.conf $HOME/.tmux.conf;
-  [ -d ~/.tmux/plugins ] || mkdir -p ~/.tmux/plugins;
-  [ -d ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  lnk "config/tmux/tmux.conf" "$HOME/.tmux.conf";
+  [ ! -d ~/.tmux/plugins ]; and mkdir -p ~/.tmux/plugins;
+  [ ! -d ~/.tmux/plugins/tpm ]; and git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 install_nvim() {
-  lnk config/nvim $XDG_CONFIG_DIR/nvim;
-  [ -d "$XDG_DATA_HOME/nvim/site/pack" ] || mkdir -p "$XDG_DATA_HOME/nvim/site/pack"
+  lnk "config/nvim" "$XDG_CONFIG_DIR/nvim";
+  [ ! -d "$XDG_DATA_HOME/nvim/site/pack" ]; and mkdir -p "$XDG_DATA_HOME/nvim/site/pack"
   lnk "share/nvim/site/pack/batpack" "$XDG_DATA_HOME/nvim/site/pack/batpack";
 }
 
 install_wezterm() {
-  lnk config/wezterm $HOME/.config/wezterm;
-  [ -e "$HOME/.config/wezterm/private_launch_menu.lua" ] || echo 'return {}' > "$HOME/.config/wezterm/private_launch_menu.lua";
-  [ -e "$HOME/.config/wezterm/private_ssh_domains.lua" ] || echo 'return {}' > "$HOME/.config/wezterm/private_ssh_domains.lua";
-  wezterm shell-completion --shell zsh > ~/.config/zsh/completion/wezterm.zsh-completion;
-  wget -qO $XDG_CONFIG_DIR/zsh/30-wezterm.sh https://raw.githubusercontent.com/wez/wezterm/main/assets/shell-integration/wezterm.sh;
+  lnk "config/wezterm" "$HOME/.config/wezterm";
+  true; and wezterm shell-completion --shell zsh > ~/.config/zsh/completion/wezterm.zsh-completion;
+  true; and curl -sL https://raw.githubusercontent.com/wez/wezterm/main/assets/shell-integration/wezterm.sh > "$XDG_CONFIG_DIR/zsh/30-wezterm.sh";
 }
 
 install_zsh() {
-  lnk config/zsh $XDG_CONFIG_DIR/zsh;
-  lnk config/starship.toml $XDG_CONFIG_DIR/starship.toml;
-  lnk config/zsh/zshrc $HOME/.zshrc;
-  [ -d ~/.config/zsh/zsh-vi-mode ] || git clone https://github.com/jeffreytse/zsh-vi-mode ~/.config/zsh/zsh-vi-mode;
+  local arch="$(uname -p)";
+  local os="$(uname -s | grep -qi linux && echo 'unknown-linux' || echo 'apple')";
+  local flavor="$(uname -o | grep -qi gnu && echo gnu || echo darwin)";
+  command -v starship > /dev/null || curl -sL "https://github.com/starship/starship/releases/latest/download/starship-$arch-$os-$flavor.tar.gz" | tar xz -C "$PWD/bin/";
+  lnk "config/zsh" "$XDG_CONFIG_DIR/zsh";
+  lnk "config/starship.toml" "$XDG_CONFIG_DIR/starship.toml";
+  lnk "config/zsh/zshrc" "$HOME/.zshrc";
+  [ ! -d ~/.config/zsh/zsh-vi-mode ]; and git clone https://github.com/jeffreytse/zsh-vi-mode ~/.config/zsh/zsh-vi-mode;
   [ -d "$HOMEBREW_PREFIX" ] \
-    && lnk $HOMEBREW_PREFIX/etc/profile.d/z.sh $XDG_CONFIG_DIR/zsh/15-z.sh;
+    && lnk "$HOMEBREW_PREFIX/etc/profile.d/z.sh" "$XDG_CONFIG_DIR/zsh/15-z.sh";
   [ -d "$HOMEBREW_PREFIX" ] \
-    && lnk $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh $XDG_CONFIG_DIR/zsh/25-zsh-syntax-highlighting.zsh
+    && lnk "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" "$XDG_CONFIG_DIR/zsh/25-zsh-syntax-highlighting.zsh";
 }
 
-XDG_CONFIG_DIR="$HOME/.config";
-XDG_DATA_HOME="$HOME/.local/share";
+and() {
+  local x="$?";
+  [ "$x" = "0" ] && echo "> $*" >&2 || echo "# $*" >&2;
+  [ "$x" = "0" ] && "$@";
+}
 
-install_zsh;
-install_wezterm;
-install_misc;
-install_tmux;
-install_nvim;
+run() {
+  echo "> $*" >&2;
+  "$@";
+}
+
+lnk() {
+  local from="$(readlink_f "$1")";
+  local to="$2";
+  [ -L "$to" ] && [ ! -r "$to" ] && run rm "$to"; # Remove broken links
+  [ ! -L "$to" ]; and ln -s "$from" "$to";
+}
+
+readlink_f() {
+  [ "x$UNAME" = "xDarwin" ] && readlink "$@" || readlink -f "$@";
+}
+
+true; and install_zsh;
+true; and install_misc;
+true; and install_tmux;
+true; and install_nvim;
+command -v wezterm > /dev/null; and install_wezterm;
