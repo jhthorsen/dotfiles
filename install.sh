f1e518cbe38939b74b529d10e661d6473418c5f2
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-10-18 13:22:10 +0900

    Doing readlink results in invalid links

--- install.sh
+++ install.sh
@@ -2,31 +2,32 @@
 
 XDG_CONFIG_DIR="$HOME/.config";
 XDG_DATA_HOME="$HOME/.local/share";
+DOTFILES="$(cd -- "$(dirname "$0")" >/dev/null ; pwd -P)";
 
 install_misc() {
-  lnk "config/ackrc" "$HOME/.ackrc";
-  lnk "config/dataprinter" "$HOME/.dataprinter";
-  lnk "config/git" "$XDG_CONFIG_DIR/git";
-  lnk "config/lf" "$XDG_CONFIG_DIR/lf";
-  lnk "config/perlcriticrc" "$HOME/.perlcriticrc";
-  lnk "config/perltidyrc" "$HOME/.perltidyrc";
+  lnk "$DOTFILES/config/ackrc" "$HOME/.ackrc";
+  lnk "$DOTFILES/config/dataprinter" "$HOME/.dataprinter";
+  lnk "$DOTFILES/config/git" "$XDG_CONFIG_DIR/git";
+  lnk "$DOTFILES/config/lf" "$XDG_CONFIG_DIR/lf";
+  lnk "$DOTFILES/config/perlcriticrc" "$HOME/.perlcriticrc";
+  lnk "$DOTFILES/config/perltidyrc" "$HOME/.perltidyrc";
 }
 
 install_tmux() {
-  lnk "config/tmux/tmux.conf" "$HOME/.tmux.conf";
+  lnk "$DOTFILES/config/tmux/tmux.conf" "$HOME/.tmux.conf";
   [ ! -d ~/.tmux/plugins ]; and mkdir -p ~/.tmux/plugins;
   [ ! -d ~/.tmux/plugins/tpm ]; and git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 install_nvim() {
-  lnk "config/nvim" "$XDG_CONFIG_DIR/nvim";
+  lnk "$DOTFILES/config/nvim" "$XDG_CONFIG_DIR/nvim";
   [ ! -d "$XDG_DATA_HOME/nvim/site/pack" ]; and mkdir -p "$XDG_DATA_HOME/nvim/site/pack"
   lnk "share/nvim/site/pack/batpack" "$XDG_DATA_HOME/nvim/site/pack/batpack";
   ./config/nvim/submodule.sh update;
 }
 
 install_wezterm() {
-  lnk "config/wezterm" "$HOME/.config/wezterm";
+  lnk "$DOTFILES/config/wezterm" "$HOME/.config/wezterm";
   true; and wezterm shell-completion --shell zsh > ~/.config/zsh/completion/wezterm.zsh-completion;
   true; and curl -sL https://raw.githubusercontent.com/wez/wezterm/main/assets/shell-integration/wezterm.sh > "$XDG_CONFIG_DIR/zsh/30-wezterm.sh";
 }
@@ -35,9 +36,9 @@ install_zsh() {
   local arch="$(uname -p)";
   local os="$(uname -s | grep -qi linux && echo 'unknown-linux' || echo 'apple')";
   local flavor="$(uname -o | grep -qi gnu && echo gnu || echo darwin)";
-  lnk "config/zsh" "$XDG_CONFIG_DIR/zsh";
-  lnk "config/starship.toml" "$XDG_CONFIG_DIR/starship.toml";
-  lnk "config/zsh/zshrc" "$HOME/.zshrc";
+  lnk "$DOTFILES/config/zsh" "$XDG_CONFIG_DIR/zsh";
+  lnk "$DOTFILES/config/starship.toml" "$XDG_CONFIG_DIR/starship.toml";
+  lnk "$DOTFILES/config/zsh/zshrc" "$HOME/.zshrc";
   [ ! -d ~/.config/zsh/zsh-vi-mode ]; and git clone https://github.com/jeffreytse/zsh-vi-mode ~/.config/zsh/zsh-vi-mode;
   [ -d "$HOMEBREW_PREFIX" ] \
     && lnk "$HOMEBREW_PREFIX/etc/profile.d/z.sh" "$XDG_CONFIG_DIR/zsh/15-z.sh";
@@ -58,14 +59,10 @@ run() {
 }
 
 lnk() {
-  local from="$(readlink_f "$1")";
+  local from="$1";
   local to="$2";
   [ -L "$to" ] && [ ! -r "$to" ] && run rm "$to"; # Remove broken links
   [ ! -L "$to" ]; and ln -s "$from" "$to";
-}
-
-readlink_f() {
-  [ "x$UNAME" = "xDarwin" ] && readlink "$@" || readlink -f "$@";
 }
 
 true; and install_zsh;
