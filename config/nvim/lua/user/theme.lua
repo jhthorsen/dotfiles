fb782a26c7f2086738f059f569745662dc155d75
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-11-13 12:26:46 +0900

    Made comments easier to read

--- config/nvim/lua/user/theme.lua
+++ config/nvim/lua/user/theme.lua
@@ -2,10 +2,19 @@ local use = require('utils').use;
 
 vim.g.spell_under = 'kanagawa'
 
+-- This comment exists to make overrides() below easier to test
+
 use('kanagawa', function(kanagawa)
   kanagawa.setup({
-    transparent = false,
+    compile = false,
+    transparent = true,
     dimInactive = true,
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        Comment = { bg = theme.ui.bg_dim, fg = theme.ui.fg_dim },
+      }
+    end,
   })
 end)
 
