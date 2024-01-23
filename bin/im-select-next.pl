8b4b72fbca11d310122c6346355fd5017834a69d
Jan Henning Thorsen <jan.henning@thorsen.pm>
2024-01-24 08:36:22 +0900

    im-select-next.pl can now take the keyboard layout as input

--- bin/im-select-next.pl
+++ bin/im-select-next.pl
@@ -2,18 +2,36 @@
 use strict;
 use warnings;
 
+my ($curr, $next) = (current_layout());
 my @lang = qw(
   com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese.Katakana
   com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese
   org.unknown.keylayout.ABCProgramming
 );
 
-my $curr = qx{/opt/homebrew/bin/im-select};
-my $next;
+@lang = grep { index("@ARGV", $_) >= 0 } @lang if @ARGV;
+
 for my $i (0..(@lang - 1)) {
   ($next = $lang[$i + 1]) && last if index($curr, $lang[$i]) == 0;
 }
-
 $next ||= $lang[0];
+
+die "Usage: $0 [name]\n" unless $next;
+print STDERR qq(Changing from "$curr" to "$next"\n) if -t STDERR;
+exit 0 if $next eq $curr;
 system '/usr/bin/osascript', -e => qq(display notification "$next" with title "im-select-next");
-system '/opt/homebrew/bin/im-select', $next;
+system '/opt/homebrew/bin/im-select' => $next;
+
+sub current_layout {
+  system 'brew install im-select' unless my $im_select = which('im-select');
+  die 'Unable to install im-select' unless $im_select ||= which('im-select');
+  my $curr = qx{$im_select};
+  chomp $curr;
+  return $curr;
+}
+
+sub which {
+  my $name = shift;
+  my @path = grep { -x "$_/$name" } (split(/:/, $ENV{PATH}), '/opt/homebrew/bin');
+  return @path ? "$path[0]/$name" : undef;
+}
