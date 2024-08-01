4e03a34e2371703749224e66eae125afc38f28fe
Jan Henning Thorsen <jan.henning@thorsen.pm>
2024-08-01 07:59:50 +0200

    Fixed bugs related to duplicates and date parsing

--- share/safari/amazon.co.jp.js
+++ share/safari/amazon.co.jp.js
@@ -1,82 +1,69 @@
 (function() {
-  const columns = ['date', 'id', 'total', 'order', 'img', 'url', 'desc'];
-  const months = ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec'];
+  const columns = ['ordered_at', 'id', 'cost', 'content', 'img_url', 'order_url', 'product_url'];
+  const $form = document.querySelector('form[action*="/your-orders/orders"]');
+  if (!$form) return console.log('Skip order parser safari/amazon.co.jp.js');
 
-  const $orders = document.querySelectorAll('.js-order-card');
-  const orders = JSON.parse(localStorage.getItem('AmazonOrderHistory') || '[]');
-  const monthRe = new RegExp(months.join('|'), 'i');
-  const rowSeen = new Set();
-  const before = orders.length;
+  let storageOrders = JSON.parse(localStorage.getItem('AmazonOrderHistory') || '[]');
+  let found = 0;
+  for (const $orderLink of document.querySelectorAll('a[href*="/order-details/"], a[href*="/order-summary"]')) {
+    const $orderCard = $orderLink.closest('.js-order-card');
+    const id = $orderCard.querySelector('.yohtmlc-order-id').textContent.replace(/.*#\s*/, '').trim(); // Remove "ORDER #"
+    if ($orderCard.textContent.match(/Return complete/)) {
+      storageOrders = storageOrders.filter(o => o.id !== id);
+      continue;
+    }
 
-  for (const $order of $orders) {
-    const $total = $order.querySelector('.yohtmlc-order-total');
-    if (!$total) continue;
+    for (const $product of $orderCard.querySelectorAll('.yohtmlc-item a')) {
+      if ($product.href.indexOf('/product/') == -1) continue;
 
-    const $orderPlaced = $order.querySelector('.a-size-base .a-color-secondary');
-    const $orderId = $order.querySelector('.yohtmlc-order-id');
-    const $itemLinks = $order.querySelectorAll('.yohtmlc-item a');
-    const $orderLink = $order.querySelector('a[href*="/order-details/"]')
-                    || $order.querySelector('a[href*="/order-summary"]')
+      // Add or update each product within an order
+      const order = storageOrders.filter(o => o.id === id && o.product_url === $product.href)[0] || {};
+      const $img = $product.closest('.a-fixed-left-grid').querySelector('img[src*="/images/"]');
 
-    const id = $orderId.textContent.replace(/.*#\s*/, '').trim(); // Remove "ORDER #"
-    const total = $total.textContent.trim().replace(/\s*\w+\s+/, '').replace(/[,.]/g, ''); // Remove "TOTAL"
+      order.content = $product.textContent.trim();
+      order.cost = $orderCard.querySelector('.yohtmlc-order-total').textContent.trim().replace(/\D+/g, '');
+      order.ordered_at = parseDate($orderCard.querySelector('.a-size-base .a-color-secondary').textContent.trim());
+      order.ts = new Date(order.ordered_at).getTime();
+      order.img_url = $img.src;
+      order.order_url = $orderLink.href;
+      order.product_url = $product.href;
 
-    if (!$orderLink) throw 'Could not find $orderLink for ' + id;
-
-    for (const $itemLink of $itemLinks) {
-      if ($itemLink.href.indexOf('/product/') == -1) continue;
-
-      const url = $itemLink.href;
-      const row = orders.filter(o => o.id === id && o.url === url)[0] || {};
-      const $img = $itemLink.closest('.a-fixed-left-grid').querySelector('img[src*="/images/"]');
-
-      if (!row.id) orders.push(row);
-      row.date = $orderPlaced.textContent.trim();
-      row.desc = $itemLink.textContent.trim();
-      row.id = id;
-      row.img = $img.src;
-      row.total = total.replace(/\D/g, '');
-      row.order = $orderLink.href;
-      row.url = url;
+      if (storageOrders.filter(o => o.id === id).length > 0) order.cost = '';
+      if (!order.id) found = storageOrders.push(order);
+      order.id = id;
     }
   }
 
-  if ($orders.length) {
-    localStorage.setItem('AmazonOrderHistory', JSON.stringify(orders));
+  const $pre = document.getElementById('AmazonOrderHistory') || document.createElement('pre');
+  $form.parentNode.insertBefore($pre, $form);
+  $pre.id = 'AmazonOrderHistory';
+  $pre.style.maxWidth = '100%';
+  $pre.style.maxHeight = '30vh';
+  $pre.style.overflow = 'auto';
+  $pre.textContent = `// total: ${storageOrders.length}\n`
+    + columns.map(k => `"${k}"`).join(",") + '\n'
+    + storageOrders.map(toRow).join('') + '\n';
 
-    const $pre = document.getElementById('AmazonOrderHistory') || document.createElement('pre');
-    $orders[0].parentNode.insertBefore($pre, $orders[0]);
-
-    $pre.id = 'AmazonOrderHistory';
-    $pre.style.maxWidth = '100%';
-    $pre.style.maxHeight = '30vh';
-    $pre.style.overflow = 'auto';
-    $pre.textContent = `// total: ${orders.length}\n` + toRow(columns) + orders.map(orderToRow).join('') + '\n';
-  }
-
-  if (before < orders.length) {
+  if (found > 0) {
+    storageOrders.sort((a, b) => b.ts - a.ts);
+    localStorage.setItem('AmazonOrderHistory', JSON.stringify(storageOrders));
     const $nextPage = document.querySelector('.a-pagination .a-last a');
     if ($nextPage) $nextPage.click();
   }
 
-  function orderToRow(order) {
-    if (rowSeen.has(order.id)) order.total = '0';
-    rowSeen.add(order.id);
-    order.date = parseDate(order.date);
-    return toRow(columns.map(k => order[k]));
-  }
-
   function parseDate(raw) {
     if (raw.match(/^\d{4}-\d{2}-\d{2}$/)) return raw;
+    const months = ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec'];
+    const monthRe = new RegExp(months.join('|'), 'i');
     const date = [];
     let parse = raw;
-    parse = parse.replace(/2\d\d\d/, (y) => date.push(y));
-    parse = parse.replace(monthRe, (m) => date.push(String(months.findIndex(name => name === m.toLowerCase()) + 1).padStart(2, '0')));
     parse = parse.replace(/\b(\d{1,2})\b/, (d) => date.push(d.padStart(2, '0')));
-    return date.length === 3 ? date.join('-') : raw.replace(/[,.]/g, '');
+    parse = parse.replace(monthRe, (m) => date.push(String(months.findIndex(name => name === m.toLowerCase()) + 1).padStart(2, '0')));
+    parse = parse.replace(/2\d\d\d/, (y) => date.push(y));
+    return date.length === 3 ? `${date[2]}-${date[1]}-${date[0]}` : raw.replace(/[,.]/g, '');
   }
 
-  function toRow(columns) {
-    return '"' + columns.map(c => String(c).replace(/["']/g, '')).join('","') + '"\n';
+  function toRow(order) {
+    return '"' + columns.map(k => String(order[k]).replace(/["']/g, '')).join('","') + '"\n';
   }
 })();
