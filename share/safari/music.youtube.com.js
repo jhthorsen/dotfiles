bd5b1d3016c8c0b83b7b9c90649ddfb315890fa2
Jan Henning Thorsen <jan.henning@thorsen.pm>
2024-11-02 10:37:27 +0900

    No need for a function inside a function

--- share/safari/music.youtube.com.js
+++ share/safari/music.youtube.com.js
@@ -1,4 +1,5 @@
-window.addEventListener('load', function() {
+// window.addEventListener('load', async function() {
+window.addEventListener('dotfiles:add_channels', async function() {
   // localStorage.setItem("dotfiles:subscriptions", "{}");
   // localStorage.removeItem("dotfiles:subscriptions");
 
@@ -8,47 +9,44 @@ window.addEventListener('load', function() {
   if (!state.channels) state.channels = {};
   if (!state.playlists) state.playlists = {};
 
-  async function addChannels() {
-
-    // Find channels (artists) in a playlist that has more than min_songs
-    if (location.pathname.indexOf('/playlist') === 0) {
-      const found = {};
-      for (const $a of await settle('ytmusic-playlist-shelf-renderer [href*="channel"]')) {
-        if (!found[$a.href]) found[$a.href] = 0;
-        found[$a.href]++;
-      }
-      for (const href in found) {
-        if (found[href] >= min_songs && !state.channels[href]) state.channels[href] = false;
-      }
-      console.info(`[music.youtube.com.js] found=${Object.keys(found).length}`, found);
-      markAsDone('playlists', location.href);
+  // Find channels (artists) in a playlist that has more than min_songs
+  if (location.pathname.indexOf('/playlist') === 0) {
+    const found = {};
+    for (const $a of await settle('ytmusic-playlist-shelf-renderer [href*="channel"]')) {
+      if (!found[$a.href]) found[$a.href] = 0;
+      found[$a.href]++;
     }
-
-    // Click the subscribe button on a channel (artist) page
-    if (location.pathname.indexOf('/channel') === 0) {
-      await new Promise(resolve => setTimeout(resolve, 500));
-      const $button = document.querySelector("#button-shape-subscribe button");
-      console.info(`[music.youtube.com.js] location=${location.href} button="${$button?.textContent}" state=${state.channels[location.href]}`);
-      if (state.channels[location.href] === false && $button && $button.textContent.indexOf('Subscribed') === -1) $button.click();
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      markAsDone('channels', location.href);
+    for (const href in found) {
+      if (found[href] >= min_songs && !state.channels[href]) state.channels[href] = false;
     }
+    console.info(`[music.youtube.com.js] found=${Object.keys(found).length}`, found);
+    markAsDone('playlists', location.href);
+  }
 
-    // Find playlists
-    if (location.pathname.indexOf('/library/playlists') === 0) {
-      for (const $playlist of await settle('a[href*="playlist?list"]')) {
-        if (!state.playlists[$playlist.href]) state.playlists[$playlist.href] = false;
-      }
-      localStorage.setItem('dotfiles:subscriptions', JSON.stringify(state));
+  // Click the subscribe button on a channel (artist) page
+  if (location.pathname.indexOf('/channel') === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+    const $button = document.querySelector("#button-shape-subscribe button");
+    console.info(`[music.youtube.com.js] location=${location.href} button="${$button?.textContent}" state=${state.channels[location.href]}`);
+    if (state.channels[location.href] === false && $button && $button.textContent.indexOf('Subscribed') === -1) $button.click();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    markAsDone('channels', location.href);
+  }
+
+  // Find playlists
+  if (location.pathname.indexOf('/library/playlists') === 0) {
+    for (const $playlist of await settle('a[href*="playlist?list"]')) {
+      if (!state.playlists[$playlist.href]) state.playlists[$playlist.href] = false;
     }
+    localStorage.setItem('dotfiles:subscriptions', JSON.stringify(state));
+  }
 
-    // Go to the next channel or playlist that has not yet been processed
-    for (const namespace of ['channels', 'playlists']) {
-      for (const href in state[namespace]) {
-        console.debug(`[music.youtube.com.js] ${state[namespace][href] ? 'done' : 'next'}=${href}`);
-        if (state[namespace][href]) continue;
-        location.href = href;
-      }
+  // Go to the next channel or playlist that has not yet been processed
+  for (const namespace of ['channels', 'playlists']) {
+    for (const href in state[namespace]) {
+      console.debug(`[music.youtube.com.js] ${state[namespace][href] ? 'done' : 'next'}=${href}`);
+      if (state[namespace][href]) continue;
+      location.href = href;
     }
   }
 
@@ -69,6 +67,7 @@ window.addEventListener('load', function() {
     }
     return found;
   }
+});
 
   addChannels();
 })
