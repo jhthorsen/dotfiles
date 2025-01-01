b5f55021f75f9041fb037c61c6b635d494b061b6
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-01-01 13:15:36 +0900

    Too difficult to move zsh around

--- config/zsh/02-proxy.sh
+++
@@ -1,7 +0,0 @@
-[ -n "$HTTPS_PROXY" -a -z "$https_proxy" ] && export https_proxy="$HTTPS_PROXY";
-[ -n "$HTTPS_PROXY" -a -z "$HTTP_PROXY"  ] && export HTTP_PROXY="$HTTPS_PROXY";
-[ -n "$HTTP_PROXY"  -a -z "$http_proxy"  ] && export http_proxy="$HTTP_PROXY";
-[ -n "$HTTP_PROXY"  -a -z "$FTP_PROXY"   ] && export FTP_PROXY="$HTTP_PROXY";
-[ -n "$FTP_PROXY"   -a -z "$ftp_proxy"   ] && export ftp_proxy="$FTP_PROXY";
-[ -n "$HTTP_PROXY"  -a -z "$NO_PROXY"    ] && export NO_PROXY="localhost,127.0.0.1"
-[ -n "$NO_PROXY"    -a -z "$no_proxy"    ] && export no_proxy="$NO_PROXY";
--- config/zsh/10-bindkey.sh
+++
@@ -1,8 +0,0 @@
-function ctrl-l {
-  echo;
-  clean;
-  zle && zle .reset-prompt && zle -R;
-}
-
-zle -N ctrl-l
-bindkey '^L' ctrl-l
