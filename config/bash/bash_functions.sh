3e631c5a498482d50658f4cde8c5386cfe1f48b4
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-01-01 13:15:51 +0900

    Made ack() into a function, instead of redefining "rg"

--- config/bash/bash_functions.sh
+++ config/bash/bash_functions.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+ack() {
+  local -a args;
+  local bin;
+
+  bin="$(command -v rg)";
+  if [ -n "$bin" ]; then
+    while [ -n "$*" ]; do case "$1" in
+      -L) args+=("--files-without-match"); shift ;;
+      *) args+=("$1"); shift ;;
+    esac done
+
+    $DRY_RUN "$bin" "${args[@]}";
+    return "$?";
+  fi
+
+  bin="$(command -v ack || command -v ack-grep)";
+  if [ -n "$bin" ]; then
+    $DRY_RUN "$bin" "$@";
+    return "$?";
+  fi
+
+  # TODO Translate arguments to grep
+  bin="grep";
+  $DRY_RUN "$bin" "$@";
+  return "$?";
+}
+
 git() {
   if [ -z "$*" ]; then command git status -bs; return "$?"; fi
   if [ ! -t 0 ]; then command git "$@"; return "$?"; fi
