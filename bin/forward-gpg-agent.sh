34d681cf474d6287841cc16adb535e0e68f4c825
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-02-25 09:07:44 +0900

    Made forward-gpg-agent less noisy

--- bin/forward-gpg-agent.sh
+++ bin/forward-gpg-agent.sh
@@ -20,16 +20,29 @@ HERE
 
 [ -z "$*" ] && usage;
 
-set -e;
-gpg -K;
+logf() {
+  local where="$1"; shift;
+  local action="$1"; shift;
+  echo "[$where] action=$action local_socket=\"$LOCAL_SOCKET\" remote_socket=\"$REMOTE_SOCKET\" $*" >/dev/stderr;
+  return 0;
+}
 
-set -x;
+sshx() {
+  ssh -xT -o ControlMaster=no "$@" || exit "$?";
+}
+
 LOCAL_SOCKET="$(gpgconf --list-dirs agent-extra-socket)";
-REMOTE_SOCKET="$(ssh -o ControlMaster=no "$@" gpgconf --list-dirs agent-socket)";
+[ -z "$LOCAL_SOCKET" ] && logf localhost discover-agent-extra-socket "gpgconf --list-dirs agent-extra-socket failed" && exit 1;
 
-[ -z "$LOCAL_SOCKET" ] && exec echo "Can't find agent-extra-socket on $(hostname)" >/dev/stderr;
-[ -z "$REMOTE_SOCKET" ] && exec echo "Can't find agent-socket on $1" >/dev/stderr;
+if [ -z "$REMOTE_SOCKET" ]; then
+  logf remotehost discover-agent-socket;
+  REMOTE_SOCKET="$(sshx "$@" 's="$(gpgconf --list-dirs agent-socket)";rm "$s" 2>/dev/null;echo "$s"')";
+else
+  logf remotehost cleanup-agent-socket;
+  sshx "$@" "rm '$REMOTE_SOCKET'";
+fi
 
-ssh -o ControlMaster=no "$@" rm -f "$REMOTE_SOCKET" \
-  && ssh -o ControlMaster=no -t -R "$REMOTE_SOCKET:$LOCAL_SOCKET" "$@" \
-    /bin/sh -c "echo Hit enter to end session; read; sleep 1; rm $REMOTE_SOCKET";
+[ -z "$LOCAL_SOCKET" ] && logf remotehost discover-agent-socket "gpgconf --list-dirs agent-socket failed" && exit 1;
+
+logf remotehost forward-agent-extra-socket;
+sshx -R "$REMOTE_SOCKET:$LOCAL_SOCKET" "$@" 'while echo "[$(date --iso-8601=seconds)] connected_to=$(hostname)";do sleep 600;done';
