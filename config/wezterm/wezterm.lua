8fdbded7c220f8b627b70914a55ebfe583eed2b7
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-03-07 08:43:45 +0900

    s/'/"/g

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm'
-local key_tables = require 'user_key_tables'
-local mouse_bindings = require 'user_mouse_bindings'
-local theme_kanagawa = require 'user_theme_kanagawa'
+local wezterm = require "wezterm"
+local key_tables = require "user_key_tables"
+local mouse_bindings = require "user_mouse_bindings"
+local theme_kanagawa = require "user_theme_kanagawa"
 
-wezterm.on('gui-startup', function()
+wezterm.on("gui-startup", function()
  local _, _, window = wezterm.mux.spawn_window({})
  window:gui_window():maximize()
 end)
 
 return {
-  audible_bell = 'Disabled',
+  audible_bell = "Disabled",
   debug_key_events = false,
   enable_scroll_bar = false,
   enable_tab_bar = false,
@@ -17,15 +17,15 @@ return {
   inactive_pane_hsb = {brightness = 0.75, saturation = 0.85},
   native_macos_fullscreen_mode = false,
   scrollback_lines = 30000,
-  window_close_confirmation = 'AlwaysPrompt',
+  window_close_confirmation = "AlwaysPrompt",
   window_padding = {top = 6, right = 6, bottom = 0, left = 6},
 
   colors = theme_kanagawa,
   font_size = 12,
   line_height = 1.3,
   font = wezterm.font_with_fallback({
-    {family = 'Hack Nerd Font'},
-    {family = 'JetBrains Mono'},
+    {family = "Hack Nerd Font"},
+    {family = "JetBrains Mono"},
   }),
 
   automatically_reload_config = true,
