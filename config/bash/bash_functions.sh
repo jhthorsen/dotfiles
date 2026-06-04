2ea3056d67a5ba7d429df7c60c981d26a84a49d9
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2026-06-04 09:52:51 +0900

    Trying to make releasing a new perl module as easy as possible

--- config/bash/bash_functions.sh
+++ config/bash/bash_functions.sh
@@ -56,6 +56,47 @@ git() {
   PAGER="diffu" command git "$@";
 }
 
+perlclean() {
+  runv make clean;
+  for f in Changes.bak Makefile META.json META.yml; do [ -e "$f" ] && runv rm "$f"; done
+  runv rm *.tar.gz 2>/dev/null;
+}
+
+perldist() {
+  command -v cpan-upload >/dev/null || cpanm -n CPAN::Uploader;
+  command -v pod2markdown >/dev/null || cpanm -n Pod::Markdown;
+
+  perlclean;
+  runv git diff --quiet && runv git diff --cached --quiet || return "$?";
+  runv git pull origin || return "$?";
+
+  local main_pm_file="$(grep ABSTRACT_FROM Makefile.PL | cut -d\' -f 2 | head -n1)";
+  local version="$(grep '[0-9]T[0-9]' ./Changes | head -n1 | awk '{print $1}')";
+  runv git rev-parse -q --verify "refs/tags/v$version" && return 1;
+  runv grep -i "Not Released" Changes && return 1;
+
+  runv find ./lib -type f -name '*pm' -exec sed -i '' -e "s/our[ ].*\$VERSION[ ]*=.*/our \$VERSION = '$version';/" '{}' \;
+  runv pod2markdown "$main_pm_file" > README.md || return "$?";
+  [ "$(git rev-list --count origin/$(git rev-parse --abbrev-ref HEAD)..HEAD)" -gt 0 ] \
+    && runv git commit -a --amend;
+
+  runv perl Makefile.PL && runv make all && runv make manifest && runv make dist || return "$?";
+  local tar="$(ls *.tar.gz)";
+  runv tar -tzf "$tar" || return "$?";
+
+  read -r -p "Do you want to run 'cpan-upload $tar' [y/N]? " answer;
+  [[ "$answer" =~ ^[Yy]$ ]] \
+    && runv git tag "v$version" && runv cpan-upload "$tar" || return "$?";
+  runv git push origin --tags;
+  runv git push origin;
+  perlclean;
+}
+
+runv() {
+  echo "$(tput setaf 2)\$ $*$(tput sgr0)" >&2
+  "$@";
+}
+
 reload() {
   "$DOTFILES_HOME/config/bash/generate.sh" "$DOTFILES_HOME/config/bash/bashrc.sh" > "$HOME/.bashrc";
   "$DOTFILES_HOME/config/bash/generate.sh" "$DOTFILES_HOME/config/bash/bash_profile.sh" > "$HOME/.bash_profile";
