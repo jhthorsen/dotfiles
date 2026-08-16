411ba01cbea969bc3d32c1db286b55e50ee2dd84
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2026-08-16 09:55:11 +0200

    nvim: Add blink.cmp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,11 @@
 vim.pack.add({
-  -- { src = "https://github.com/saghen/blink.cmp", version = "v1" },
-  -- { src = "https://github.com/fang2hou/blink-copilot" },
+  { src = "https://github.com/saghen/blink.cmp",                    version = "v1" },
+  { src = "https://github.com/fang2hou/blink-copilot" },
   { src = "https://github.com/uga-rosa/ccc.nvim" },
   { src = "https://github.com/olimorris/codecompanion.nvim" },
   { src = "https://github.com/ravitemer/codecompanion-history.nvim" },
   { src = "https://github.com/zbirenbaum/copilot.lua" },
-  -- { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
   { src = "https://github.com/ravitemer/mcphub.nvim" },
   -- { src = "https://github.com/echasnovski/mini.nvim" },
   { src = "https://github.com/jake-stewart/multicursor.nvim" },
@@ -214,6 +214,69 @@ require("which-key").add({
   { "<leader>m", group = "Multicursors...", icon = "󰘞" },
   { "<leader>n", group = "Neovim...", icon = "" },
   { "<leader>q", group = "Quit...", icon = "󰩈" },
+})
+
+----------------------------------------------------------------------------------------------------
+-- Blink - Autocomplete
+----------------------------------------------------------------------------------------------------
+require("blink.cmp").setup({
+  completion = {
+    documentation = { auto_show = true, auto_show_delay_ms = 750 },
+    menu = {
+      draw = {
+        columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+        treesitter = { "lsp" }
+      },
+    },
+  },
+  keymap = {
+    preset = "default",
+    ["<tab>"] = { function(ctx) return ctx.select_next() end, "fallback" },
+    ["<s-tab>"] = { function(ctx) return ctx.select_prev() end, "fallback" },
+    ["<cr>"] = {
+      function(ctx)
+        local item = ctx.get_selected_item() or {}
+        if item.source_id == "copilot" then return ctx.select_and_accept() end
+        if item.source_id == "snippets" then return ctx.select_and_accept() end
+
+        local col = vim.api.nvim_win_get_cursor(0)[2]
+        if col == 0 then return nil end
+
+        local line = vim.api.nvim_get_current_line()
+        local word_before = line:sub(col, col):match("[%w_.]")
+        if word_before ~= nil then return ctx.select_and_accept() end
+      end,
+      "fallback",
+    },
+  },
+  sources = {
+    default = {
+      "buffer",
+      "lsp",
+      "snippets",
+      "path",
+      "copilot",
+      "codecompanion",
+    },
+    providers = {
+      codecompanion = {
+        name = "CodeCompanion",
+        module = "codecompanion.providers.completion.blink",
+      },
+      copilot = {
+        name = "copilot",
+        module = "blink-copilot",
+        min_keyword_length = 2,
+        async = true,
+        score_offset = 50,
+        opts = {
+          debounce = 500,
+          max_completions = 2,
+          max_attempts = 2,
+        },
+      },
+    },
+  },
 })
 
 ----------------------------------------------------------------------------------------------------
