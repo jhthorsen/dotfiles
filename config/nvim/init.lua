41e889b9873b97522381ed5d1c4534874fcff64d
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2026-08-17 16:52:50 +0200

    Fix file picker background

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -664,8 +664,11 @@ require("nvim-treesitter").install({
 -- Theme
 ----------------------------------------------------------------------------------------------------
 vim.cmd.colorscheme("catppuccin")
-vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#89b4fa", bg = "NONE" })
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#89b4fa", bg = "#181825" })
+vim.api.nvim_set_hl(0, "FloatNormal", { fg = "#89b4fa", bg = "#181825" })
+vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#89b4fa", bg = "#181825" })
+vim.api.nvim_set_hl(0, "MiniFilesNormal", { bg = "#181825" })
 
 ----------------------------------------------------------------------------------------------------
 -- Venn - Draw ASCII diagrams
